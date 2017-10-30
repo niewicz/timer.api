2d965d82e0997d36f948bfbfc257d315c74b4257
Tomasz Próchniewicz <tommpoland@gmail.com>
2017-10-30 09:04:40 +0100

    user config

--- db/migrate/20171029202251_devise_token_auth_create_users.rb
+++ db/migrate/20171029202251_devise_token_auth_create_users.rb
@@ -28,14 +28,9 @@ class DeviseTokenAuthCreateUsers < ActiveRecord::Migration[5.1]
       t.datetime :confirmation_sent_at
       t.string   :unconfirmed_email # Only if using reconfirmable
 
-      ## Lockable
-      # t.integer  :failed_attempts, :default => 0, :null => false # Only if lock strategy is :failed_attempts
-      # t.string   :unlock_token # Only if unlock strategy is :email or :both
-      # t.datetime :locked_at
-
       ## User Info
-      t.string :name
-      t.string :nickname
+      t.string :first_name
+      t.string :last_name
       t.string :image
       t.string :email
 
@@ -49,6 +44,5 @@ class DeviseTokenAuthCreateUsers < ActiveRecord::Migration[5.1]
     add_index :users, [:uid, :provider],     unique: true
     add_index :users, :reset_password_token, unique: true
     add_index :users, :confirmation_token,   unique: true
-    # add_index :users, :unlock_token,       unique: true
   end
 end
