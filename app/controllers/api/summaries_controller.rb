b806f71441023e2327ce4a1d84cee91869b2592d
Tomasz Próchniewicz <tomasz.prochniewicz@prowly.com>
2018-01-21 23:22:40 +0100

    improvent chart data factory

--- app/controllers/api/summaries_controller.rb
+++ app/controllers/api/summaries_controller.rb
@@ -1,16 +1,28 @@
 class Api::SummariesController < ApplicationController
 
   def workload_chart
-    current_user.time_entries
-      .where('end_at is not null')
-      .where('start_at > ?', Time.zone.now.utc.beginning_of_day - 7.days)
-      .group_by(&:day_of_entry)
-      .map{ |g| 
-        { 
-          date: g[0],
-          data: Time.at(g[1].map{ |te| te.total_time }.reduce(:+)).utc.strftime('%H:%M')
-        }
-      }
+    range_start = Time.now.utc.beginning_of_day - 6.days
+    range_end = Time.now.utc.beginning_of_day
+
+    results = []
+
+    (range_start.to_i..range_end.to_i).step(1.days.to_i) do |date_i|
+      total = 0
+
+      entries = current_user.time_entries
+        .where('end_at IS NOT NULL')
+        .where('start_at > ? AND start_at < ?', 
+          Time.at(date_i), Time.at(date_i).end_of_day
+        )
+
+      if entries.present?
+        total = entries.map{ |te| te.total_time }.reduce(:+)
+      end
+
+      results << { name: Time.at(date_i).to_date.to_s, value: total }
+    end
+
+    render json: { data: results }
   end
 
 end
