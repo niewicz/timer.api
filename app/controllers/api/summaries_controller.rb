ab7e4ba4e29331f2b509927c19da2da0f95483fc
Tomasz Próchniewicz <tomasz.prochniewicz@prowly.com>
2018-03-31 23:52:05 +0200

    summaries chart

--- app/controllers/api/summaries_controller.rb
+++ app/controllers/api/summaries_controller.rb
@@ -1,28 +1,12 @@
 class Api::SummariesController < ApplicationController
 
   def workload_chart
-    range_start = Time.now.utc.beginning_of_day - 6.days
-    range_end = Time.now.utc.beginning_of_day
+    results = ::Timer::Summaries::Workload.new(current_user, params).call
 
-    results = []
-
-    (range_start.to_i..range_end.to_i).step(1.days.to_i) do |date_i|
-      total = 0
-
-      entries = current_user.time_entries
-        .where('end_at IS NOT NULL')
-        .where('start_at > ? AND start_at < ?', 
-          Time.at(date_i), Time.at(date_i).end_of_day
-        )
-
-      if entries.present?
-        total = entries.map{ |te| te.total_time }.reduce(:+)
-      end
-
-      results << { name: Time.at(date_i).to_date.to_s, value: total }
-    end
-
-    render json: { data: results }
+    render json: { 
+      data: results, 
+      total: results.map{ |i| i[:value] }.reduce(:+) 
+    }
   end
 
 end
---
+++ app/services/timer/summaries/workload.rb
@@ -0,0 +1,52 @@
+class Timer::Summaries::Workload
+
+  def initialize(user, params)
+    @user = user.presence || fail(ArgumentError)
+    @params = params.presence || fail(ArgumentError)
+  end
+
+  def call
+    prepare_params!
+
+    results = []
+
+    (@range_start.to_i..@range_end.to_i).step(1.days.to_i) do |date_i|
+      total = 0
+
+      start_i = Time.zone.at(date_i)
+      end_i = Time.zone.at(date_i).end_of_day
+
+      entries = @user.time_entries
+        .where('end_at IS NOT NULL')
+        .where('start_at >= ? AND start_at <= ? OR end_at >= ? AND end_at <= ? OR start_at < ? AND end_at > ?', 
+          start_i, end_i, start_i, end_i, start_i, end_i
+        )
+
+      if entries.present?
+        total = entries.map do |te| 
+          if te.start_at >= start_i && te.end_at <= end_i
+            te.total_time 
+          elsif te.start_at >= start_i && te.end_at > end_i
+            TimeEntry.new(start_at: te.start_at, end_at: end_i).total_time
+          elsif te.start_at < start_i && te.end_at < end_i
+            TimeEntry.new(start_at: start_i, end_at: te.end_at).total_time
+          else
+            24.hours.to_i
+          end
+        end.reduce(:+)
+      end
+
+      results << { name: Time.at(date_i).to_date.to_s, value: total }
+    end
+
+    results
+  end
+
+  private 
+  
+  def prepare_params!
+    @range_start = @params[:day_start] ? Time.zone.parse(@params[:day_start]) : Time.zone.now.beginning_of_day - 6.days
+    @range_end = @params[:day_end] ? Time.zone.parse(@params[:day_end]) : Time.zone.now.beginning_of_day
+  end
+
+end
