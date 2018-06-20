1d774b9262450de1ace146a22b8aa968e0ec0f7d
Tomasz Próchniewicz <tomasz.prochniewicz@prowly.com>
2018-06-20 19:54:20 +0200

    add mailler config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,16 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Mailer configuration
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'example.com',
+    user_name:            Figaro.env.report_mailer_email,
+    password:             Figaro.env.report_mailer_password,
+    authentication:       'plain',
+    enable_starttls_auto: true 
+  }
 end
